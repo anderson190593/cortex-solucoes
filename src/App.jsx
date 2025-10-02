3ca2211eda3b21ac9e2877794db72fdb1fec861f
Anderson Roberto <146346145+anderson190593@users.noreply.github.com>
2025-10-02 10:15:57 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@
 import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import HeroSection from './components/HeroSection/HeroSection'; // 1. Importa o HeroSection
+import HeroSection from './components/HeroSection/HeroSection';
+import Services from './components/Services/Services'; // 1. Importa o Services
 
 function App() {
   const [theme, setTheme] = useState('dark');
@@ -14,7 +15,8 @@ function App() {
   return (
     <div className="App" data-theme={theme}>
       <Navbar theme={theme} toggleTheme={toggleTheme} />
-      <HeroSection /> {/* 2. Adiciona o componente aqui */}
+      <HeroSection />
+      <Services /> {/* 2. Adiciona o componente aqui */}
     </div>
   );
 }
