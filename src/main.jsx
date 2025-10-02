d69a22a3abaa1d8f06c47db081b709a9203293a6
Anderson Roberto <146346145+anderson190593@users.noreply.github.com>
2025-10-02 00:02:11 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'; // <-- Adicione esta linha aqui
 import App from './App.jsx'
+import './index.css' // (A ordem deste não importa muito agora)
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
