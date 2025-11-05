de2e075fd63e5fa78c1c014734a195393937f1f5
Anderson Roberto <146346145+anderson190593@users.noreply.github.com>
2025-11-05 00:17:21 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 // src/App.jsx
 import { useState } from 'react';
-import './App.css'; // Este é o caminho relativo correto
-import Navbar from './components/Navbar/Navbar'; // Este é o caminho relativo correto
-import HeroSection from './components/HeroSection/HeroSection'; // Este é o caminho relativo correto
+import './App.css'; 
+import Navbar from './components/Navbar/Navbar'; 
+import HeroSection from './components/HeroSection/HeroSection';
+import Features from './components/Features/Features';
+import ProjectControl from './components/ProjectControl/ProjectControl';
+import Portfolio from './components/Portfolio/Portfolio';
+import CTA from './components/CTA/CTA';
+import Footer from './components/Footer/Footer';
 
 function App() {
   const [theme, setTheme] = useState('dark');
@@ -15,6 +20,11 @@ function App() {
     <div className="App" data-theme={theme}>
       <Navbar theme={theme} toggleTheme={toggleTheme} />
       <HeroSection />
+      <Features />
+      <ProjectControl />
+      <Portfolio />
+      <CTA />
+      <Footer />
     </div>
   );
 }
